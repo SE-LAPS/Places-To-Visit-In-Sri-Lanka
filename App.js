0d8b30faaae4cae1be48634a035e5aa2d73a1b61
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2023-03-08 11:28:00 +0530

    Travel Web Site Using React.js
    Travel Web Site Using React.js

---
+++ App.js
@@ -0,0 +1,283 @@
+import React from "react";
+import ImageContentHover from 'react-image-hover';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "./App.css";
+
+
+export default function App() {
+  return (
+    <>
+    <center>
+      <h1>
+       "15" MOST Beautiful
+      </h1>
+      <h2>
+        Places In
+      </h2>
+      <h3>
+        Sri Lanka
+      </h3>
+      </center>
+
+      <br /><br />
+      <
+      Swiper watchSlidesProgress={true} slidesPerView={3} className="mySwiper"
+      className="mySwiper2"
+      >
+      
+  
+         
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image ="https://pyt-blogs.imgix.net/2020/05/yves-alarie-3R50kTNBKiE-unsplash-scaled.jpg?auto=format&ixlib=php-3.3.0"
+         content={{
+            title: 'Place No: 01',
+            body: 'Image content viewer'
+          }} 
+          effect="slideVertical" />
+      </div>
+        </SwiperSlide>
+
+
+        <br></br>
+        <div class="text"><center>Sri Lankan Beautiful Places😍</center></div>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://www.pearldestinations.com/images/elephant-orphanage-feeding-sri-lanka.jpg"
+        content={{
+            title: 'Place No: 02',
+            body: 'Image content viewer'
+          }} 
+          effect="slideHorizontal" />
+      </div>
+        </SwiperSlide>
+        
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://c.myholidays.com/blog/blog/content/images/2021/04/Polonnaruwa.jpg"
+          content={{
+            title: 'Place No: 03',
+            body: 'Image content viewer'
+          }} 
+          effect="slideVertical" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://www.tripsavvy.com/thmb/20TsznxhuzvNFmtIkffrdOeVQm4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/aerial-view-of-sigiriya-rock-at-misty-morning--sri-lanka--drone-photo--1129567907-a6628ce7d636462f9a0e0361a3808178.jpg"
+         content={{
+            title: 'Place No: 04',
+            body: 'Image content viewer'
+          }} 
+          effect="slideHorizontal" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://sandinmysuitcase.com/wp-content/uploads/2021/04/Kandy-Sri-Lanka.jpg.webp"
+        content={{
+            title: 'Place No: 05',
+            body: 'Image content viewer'
+          }} 
+          effect="slideVertical" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://media.tacdn.com/media/attractions-splice-spp-674x446/07/ba/da/dd.jpg" 
+        content={{
+            title: 'Place No: 06',
+            body: 'Image content viewer'
+          }} 
+          effect="slideHorizontal" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://assets.traveltriangle.com/blog/wp-content/uploads/2014/11/Dambulla-Cave-Temple_23rd-oct.jpg" 
+         content={{
+            title: 'Place No: 07',
+            body: 'Image content viewer'
+          }} 
+          effect="slideVertical" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://destinationlesstravel.com/wp-content/uploads/2019/04/DSC_8786-1024x684.jpg" 
+        content={{
+            title: 'Place No: 08',
+            body: 'Image content viewer'
+          }} 
+          effect="slideHorizontal" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px"
+          image="https://tourscanner.com/blog/wp-content/uploads/2018/05/Traditional-Fishermen-in-Sri-Lanka-Sunset-Places-to-Visit-Sri-Lanka.jpg"
+         content={{
+            title: 'Place No: 09',
+            body: 'Image content viewer'
+          }} 
+          effect="slideVertical" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://lp-cms-production.imgix.net/image_browser/exploring-madu-river-sri-lanka.jpg" 
+        content={{
+            title: 'Place No: 10',
+            body: 'Image content viewer'
+          }} 
+          effect="slideHorizontal" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image ="https://srilankanexpeditions.de/images/main_slider/destination/01.jpg" 
+         content={{
+            title: 'Place No: 11',
+            body: 'Image content viewer'
+          }} 
+          effect="slideVertical" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://tourscanner.com/blog/wp-content/uploads/2018/05/Adams-Peak-Aerial-View-Places-to-Visit-Sri-Lanka.jpg" 
+         content={{
+            title: 'Place No: 12',
+            body: 'Image content viewer'
+          }} 
+          effect="slideHorizontal" />
+      </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://cdn-pub.piksyn.com/ccfc6a0c4bb18b354857d4c25ec7bc9c58630dd36e5035182229d905cfc1a4fb/ec81c2306c308e2ab3239491df9e020ce6a3fcc8.jpg" 
+         content={{
+            title: 'Place No: 13',
+            body: 'Image content viewer'
+          }} 
+          effect="slideVertical" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://cdn.kimkim.com/files/a/content_articles/featured_photos/bae18ebe06aa58356b3487eb6dec13ac43d717ee/big-08881dfe8293dbf49028db10b2063791.jpg" 
+        content={{
+            title: 'Place No: 14',
+            body: 'Image content viewer'
+          }} 
+          effect="slideHorizontal" />
+      </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+        <div className="App">
+        <ImageContentHover 
+          className="image-cover"
+          tileWidth="130px"
+          tileHeight="350px" 
+          image="https://destinationlesstravel.com/wp-content/uploads/2019/05/DSC_0299.jpg"
+        content={{
+            title: 'Place No: 15',
+            body: 'Image content viewer'
+          }} 
+          effect="slideVertical" />
+      </div>
+        </SwiperSlide>
+
+
+      </Swiper>
+      
+    </>
+  );
+}
